0b3155fa48f4f15c5d32f68ab83d63112f22464a
André Baudin-Laurencin <abaudin@gmail.com>
2015-02-11 15:23:38 +0100

    recherche ok sur deux champs de agence

--- myapp/client/views/agences/agences_search.js
+++ myapp/client/views/agences/agences_search.js
@@ -1,18 +1,20 @@
 
 
-//Session.set('resultat' , undefined);
+Session.set('resultat' , undefined);
 
 Template.agencesSearch.events({
 //FORMULAIRE
     'submit form': function (e) {
         e.preventDefault();
         var agencesSearch = {
-            raisonSocialeAgence: $("#searchTerms").val(),
+            raisonSocialeAgence: $(e.target).find('[name=raisonSocialeAgence]').val(),
+            ville: $(e.target).find('[name=ville]').val(),
             userId:Meteor.user()
         };
 
         //Chargement des entrées formulaire dans la session
         Session.set('resultat', agencesSearch);
+//alert([agencesSearch.raisonSocialeAgence] + [agencesSearch.ville])
 
     }
 });
@@ -23,8 +25,11 @@ Template.agencesSearch.helpers({
     searchResult:function(){
 
         var search = Session.get('resultat'); // Récupere les valeurs entrées dans le formulaire par la session
+        alert([search.raisonSocialeAgence] + [search.ville])
+        var agencesFind =
+                Agences.find({$or: [{raisonSocialeAgence:String(search.raisonSocialeAgence)}, {ville:String(search.ville)}]} )
 
-        var agencesFind = Agences.find({raisonSocialeAgence:String(search.raisonSocialeAgence)}); // Requete db
+            ; // Requete db
 
         return agencesFind ;
     }
