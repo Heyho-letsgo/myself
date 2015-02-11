64b33bbafe6dccb4182be4be196f8c2e4d13dbb8
André Baudin-Laurencin <abaudin@gmail.com>
2015-02-11 13:31:13 +0100

    Validation de la recherche sur agence

--- myapp/client/views/agences/agences_search.js
+++ myapp/client/views/agences/agences_search.js
@@ -1,24 +1,32 @@
 
+
+//Session.set('resultat' , undefined);
+
 Template.agencesSearch.events({
+//FORMULAIRE
     'submit form': function (e) {
         e.preventDefault();
         var agencesSearch = {
             raisonSocialeAgence: $("#searchTerms").val(),
             userId:Meteor.user()
         };
-        var agencesFind = Agences.find();
 
-//        alert(agencesFind)
-
-        //_.each(agencesFind, alert);
-
-
-        return Router.go('agencesSearch') ;
-
-
-
+        //Chargement des entrées formulaire dans la session
+        Session.set('resultat', agencesSearch);
 
     }
 });
 
 
+Template.agencesSearch.helpers({
+
+    searchResult:function(){
+
+        var search = Session.get('resultat'); // Récupere les valeurs entrées dans le formulaire par la session
+
+        var agencesFind = Agences.find({raisonSocialeAgence:String(search.raisonSocialeAgence)}); // Requete db
+
+        return agencesFind ;
+    }
+
+});
