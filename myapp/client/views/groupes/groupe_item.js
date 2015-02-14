4ce030bf06a3ef78abf16696787a64f64e172a4c
André Baudin-Laurencin <abaudin@gmail.com>
2015-02-14 12:08:11 +0100

    Rajout de commentaire

--- myapp/client/views/groupes/groupe_item.js
+++ myapp/client/views/groupes/groupe_item.js
@@ -5,33 +5,30 @@ Template.groupeItem.events({
         }
 
 });
-/**
- * Created by andrew on 09/02/15.
- */
-
 
 
 Template.groupeItem.events({
 'click .addAgence': function () {
-    //var currentUserId = this.userId;
+    // Je récupère l'id du groupe en cours et la valeur du champ raisonSociale.
         var idGroupeSearch =  this._id;
         var groupeName = this.raisonSociale;
 
-alert( groupeName)
-
-        //Session.set('resultatId',idGroupeSearch);
-        //Session.set('groupName', groupeName ) ;
-
+//alert( groupeName)
+    // je crée une nouvelle agence en passant les champs ci-dessous.
         Agences.insert({
             userId:Meteor.user(),
             groupeId: idGroupeSearch,
             groupeRaisonSociale: groupeName,
+            //Je crée un champ temporaire et lui affecte une valeur --> à transformer en valeur random
             agenceNew: 1
         });
 
         //var agenceCourante = Agences.findOne({}, {sort:{groupeId:idGroupeSearch}});
-    var agenceCourante = Agences.findOne({agenceNew : 1});
 
+
+    // Je crée la variable qui me permet de passer l'Url de la route
+    var agenceCourante = Agences.findOne({agenceNew : 1});
+    // Je vais sur l'Url.
         Router.go("agenceEdit", { _id: agenceCourante._id}) ;
 
     }
@@ -39,23 +36,3 @@ alert( groupeName)
 });
 
 
-
-        /*  var agencesFind =
-         Agences.find({$or: [
-         {raisonSocialeAgence:String(search.raisonSocialeAgence)},
-         {ville:String(search.ville)},
-         {type:String(search.type)}
-
-         ]} )
-
-         ; // Requete db
-
-         return agencesFind ;
-         */
-
-
-
-
-
-       // alert ([idGroupeSearch.raisonSociale])
-        //
