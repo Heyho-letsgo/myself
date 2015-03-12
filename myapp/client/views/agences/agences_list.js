d0c7e0de64c09dfe8b1cd3cdd880397faea50db2
Andy <abaudin@gmail.com>
2015-03-12 10:59:33 +0000

    Allegement et remise en forme du fichier

--- myapp/client/views/agences/agences_list.js
+++ myapp/client/views/agences/agences_list.js
@@ -5,52 +5,20 @@ Template.agencesList.helpers({
                 sort: {raisonSocialeAgence: -1}, limit: 5
             });
     },
-      totalAgences: function () {
-    	return Agences.find().count();
-	}
-});
-
-
-Template.agencesList.helpers({
-    agences: function () {
-        return Agences.find();
-    }
-});
-
-Template.agencesList.helpers({
     agencesTotalParUttilisateur: function () {
         return Agences.find(
             {
                 userId: { $in : [Meteor.user()]}}
         ).count();
-    }
-});
-
-Template.agencesList.helpers({
+    },
     agencesTotal: function () {
         return Agences.find().count();
-    }
-});
-
-
-
-
-Template.agencesList.helpers({
+    },
     username: function () {
         return Meteor.user() && Meteor.user()._id;
-    }
-});
-
-
-Template.agencesList.helpers({
+    },
     agencesParUtilisateur: function () {
         return Agences.find({
             userId: { $in : [Meteor.user()]}});
     }
-});
-
-
-
-
-
-
+  });
