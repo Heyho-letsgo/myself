0f0f2271d0048c527ff5401019a7a67ebe07e5ee
André Baudin-Laurencin <abaudin@gmail.com>
2015-04-03 09:34:16 +0200

    Mise en commentaire de ce fichier

--- myapp/server/fixtures.js
+++ myapp/server/fixtures.js
@@ -1,3 +1,4 @@
+/*
 if (Groupes.find().count() === 0) {
     Groupes.insert({raisonSociale: 'GroupeImmo01'});
     Groupes.insert({raisonSociale: 'GroupeImmo02'});
@@ -22,3 +23,4 @@ if (Agences.find().count() === 0) {
     Agences.insert({ville: 'Paris', type: 'agence', raisonSocialeAgence: 'Agence02'});
     Agences.insert({ville: 'Paris', type: 'agence', raisonSocialeAgence: 'Agence03', adresse: '48 rue des Moines'});
 }
+*/
